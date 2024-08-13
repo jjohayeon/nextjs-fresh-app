4a4d60bee32e02dadfa77d67c3b5ec9252745d2d
jjohayeon <jjohayeon@naver.com>
2024-08-13 21:12:24 +0900

    fresh-app Part1

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Cart = () => {
-    return (
-        <h1>
-          장바구니 페이지 입니다.
-        </h1>
-    );
+  return (
+    <>
+      <div className="cartItemWrap">
+        <div className="cartItem">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </div>
+        <div className="cartItem">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
-
 export default function List() {
-  let product: string[] = ['Tomato', 'Pasta', 'Coconut']
+  let product: string[] = ["Tomato", "Pasta", "Coconut"];
 
   return (
     <div className="mainWrap">
       <h4>List Page</h4>
       <div>
-        {product.map((item, i) => (
-          <div className="food" key={i}>
-
-            <h4>{item} $40</h4>
-          </div>
-        ))}
+        {product.map((item, i) => {
+          return (
+            <div className="food" key={i}>
+              <img src={`food${i}.png`} />
+              <h4>{item} $40</h4>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
